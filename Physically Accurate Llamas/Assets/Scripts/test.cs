40ccbcfc94b92c374b87a2ab9b8d02876f525012
Ben Laycock <benlaycock17@gmail.com>
2020-10-13 11:52:43 +0100

    test

---
+++ Physically Accurate Llamas/Assets/Scripts/test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    private int one;
+    private int one1;
+    private int one2;
+    private int one3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (one == one1)
+        {
+            one1 = one2;
+        }
+    }
+}
