6a747f6196300e993e4077e71fb45865ea22aa0f
treecreeper123 <treecreeper123@gmail.com>
2020-10-25 23:56:20 +0000

    Main Menu
    First Main Menu Iteration

---
+++ Physically Accurate Llamas/Assets/Scripts/Menus/MainMenuManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour 
+{
+
+    [SerializeField] private bool mLoadToTestingLevel = false;
+
+    [SerializeField] private float mMenuSlideSpeedModifier = 1.0f;
+
+    [SerializeField] private GameObject mMainMenuObject;
+    [SerializeField] private GameObject mOptionsMenuObject;
+
+    private float mMenuMoveTimer = 0.0f;
+    private bool mSwitchToOptionsMenu = false;
+
+    private Vector3 mMainMenuOriginalPosition;
+    private Vector3 mOptionsMenuOriginalPosition;
+
+    private RectTransform mMainMenuObjectRectTransform;
+    private RectTransform mOptionsMenuObjectRectTransform;
+
+
+    private void Start()
+    {
+
+        mMainMenuObjectRectTransform = mMainMenuObject.GetComponent<RectTransform>();
+        mOptionsMenuObjectRectTransform = mOptionsMenuObject.GetComponent<RectTransform>();
+
+        mMainMenuOriginalPosition = mMainMenuObjectRectTransform.position;
+        mOptionsMenuOriginalPosition = mOptionsMenuObjectRectTransform.position;
+
+    }
+
+
+    public void OnPlayButtonPressed()
+    {
+
+        if(mLoadToTestingLevel)
+        {
+            SceneManager.LoadSceneAsync("MTL", LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync("Level1", LoadSceneMode.Single);
+        }
+
+    }
+
+
+    public void OnOptionsButtonPressed()
+    {
+
+        if(!mSwitchToOptionsMenu)
+        {
+            mMenuMoveTimer = 0.0f;
+            mSwitchToOptionsMenu = true;
+        }
+
+    }
+
+
+    public void OnExitButtonPressed()
+    {
+
+        Application.Quit();
+
+    }
+
+
+    private void Update()
+    {
+        
+        if(mSwitchToOptionsMenu)
+        {
+
+            mMenuMoveTimer += Time.deltaTime * mMenuSlideSpeedModifier;
+
+            mMainMenuObjectRectTransform.position = Vector3.Lerp(mMainMenuOriginalPosition, mMainMenuOriginalPosition + new Vector3(-Screen.width, 0, 0), Mathf.Clamp(mMenuMoveTimer, 0.0f, 1.0f));
+            mOptionsMenuObjectRectTransform.position = Vector3.Lerp(mOptionsMenuOriginalPosition, mOptionsMenuOriginalPosition + new Vector3(-Screen.width, 0, 0), Mathf.Clamp(mMenuMoveTimer, 0.0f, 1.0f));
+
+            if((mMainMenuObjectRectTransform.position - (mMainMenuOriginalPosition + new Vector3(-Screen.width, 0, 0))).magnitude <= 0.05)
+            {
+                mSwitchToOptionsMenu = false;
+            }
+
+        }
+
+    }
+
+}
---
+++ Physically Accurate Llamas/Assets/Scripts/Menus/OptionsMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    [SerializeField] private float mMenuSlideSpeedModifier = 1.0f;
+
+    [SerializeField] private GameObject mMainMenuObject;
+    [SerializeField] private GameObject mOptionsMenuObject;
+
+    private float mMenuMoveTimer = 0.0f;
+    private bool mSwitchToMainMenu = false;
+
+    private Vector3 mMainMenuOriginalPosition;
+    private Vector3 mOptionsMenuOriginalPosition;
+
+    private RectTransform mMainMenuObjectRectTransform;
+    private RectTransform mOptionsMenuObjectRectTransform;
+
+
+    private void Start()
+    {
+
+        mMainMenuObjectRectTransform = mMainMenuObject.GetComponent<RectTransform>();
+        mOptionsMenuObjectRectTransform = mOptionsMenuObject.GetComponent<RectTransform>();
+
+        mMainMenuOriginalPosition = mMainMenuObjectRectTransform.position;
+        mOptionsMenuOriginalPosition = mOptionsMenuObjectRectTransform.position;
+
+    }
+
+
+    public void OnBackButtonPressed()
+    {
+
+        if (!mSwitchToMainMenu)
+        {
+            mMenuMoveTimer = 0.0f;
+            mSwitchToMainMenu = true;
+        }
+
+    }
+
+
+    private void Update()
+    {
+
+        if (mSwitchToMainMenu)
+        {
+
+            mMenuMoveTimer += Time.deltaTime * mMenuSlideSpeedModifier;
+
+            mMainMenuObjectRectTransform.position = Vector3.Lerp(mMainMenuOriginalPosition + new Vector3(-Screen.width, 0, 0), mMainMenuOriginalPosition, Mathf.Clamp(mMenuMoveTimer, 0.0f, 1.0f));
+            mOptionsMenuObjectRectTransform.position = Vector3.Lerp(mOptionsMenuOriginalPosition+ new Vector3(-Screen.width, 0, 0), mOptionsMenuOriginalPosition, Mathf.Clamp(mMenuMoveTimer, 0.0f, 1.0f));
+
+            if ((mMainMenuObjectRectTransform.position - mMainMenuOriginalPosition).magnitude <= 0.05)
+            {
+                mSwitchToMainMenu = false;
+            }
+
+        }
+
+    }
+
+}
