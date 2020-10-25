bd0a3d6661e7e6f37cba81eca753bf342964248f
Ben Laycock <benlaycock17@gmail.com>
2020-10-25 23:14:04 +0000

    Base Systems
    Added:
     - PoolSystem
     - AudioSystem
     - DeactivateObjectAfterTime

---
+++ Physically Accurate Llamas/Assets/Scripts/Helpers/DeactivateObjectAfterTime.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateObjectAfterTime : MonoBehaviour
+{
+
+    private float mDeactivationTime = 0f;
+    public float DeactivationTime
+    {
+
+        set
+        {
+            mDeactivationTime = value;
+        }
+        get
+        {
+            return mDeactivationTime;
+        }
+
+    }
+
+
+    private float mTimer = 0f;
+    private bool mTimerActive = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (mTimerActive)
+        {
+            mTimer -= Time.deltaTime;
+
+            if (0f >= mTimer)
+                ManageDeactivation();
+        }
+
+    }
+
+
+    private void ManageDeactivation()
+    {
+
+        DeactivateTimer();
+        ResetTimer();
+
+        this.gameObject.SetActive(false);
+
+    }
+
+
+    /*
+     * Resets the timer to the Deactivation Time current stored
+     */
+    public void ResetTimer()
+    {
+
+        mTimer = mDeactivationTime;
+
+    }
+
+
+    /*
+     * Starts the timer countdown until object deactivation
+     */
+    public void ActivateTimer()
+    {
+
+        mTimerActive = true;
+
+    }
+
+
+    /*
+     * Stops the timer countdown
+     */
+    public void DeactivateTimer()
+    {
+
+        mTimerActive = false;
+
+    }
+
+}
