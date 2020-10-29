0f73c861af14a4f4acb69ab4b66dbf5b3402c2d8
WesleyBainbridge <wbainbridge1999@gmail.com>
2020-10-29 17:51:24 +0000

    Enemy Mechanics

---
+++ Physically Accurate Llamas/Assets/Scripts/Systems/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy: MonoBehaviour
+{
+    [SerializeField] public float mLookArea = 10f;
+
+    Transform mPlayer;
+    NavMeshAgent mAgent;
+
+
+    private void Start ()
+    {
+        mPlayer = PlayerTarget.instance.mPlayer.transform;
+        mAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(mPlayer.position, transform.position);
+
+        if (distance <= mLookArea)
+        {
+            mAgent.SetDestination(mPlayer.position);
+
+           
+           if (distance <= mAgent.stoppingDistance)
+            {
+                FacePlayer();
+
+            } 
+
+        }
+    
+    }
+
+    void FacePlayer ()
+    {
+        Vector3 direction = (mPlayer.position - transform.position).normalized;
+        Quaternion lookRoatation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRoatation, Time.deltaTime * 5f);
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, mLookArea);
+    }
+
+
+}
---
+++ Physically Accurate Llamas/Assets/Scripts/Systems/PlayerTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTarget: MonoBehaviour
+{
+
+    #region Singleton
+
+    public static PlayerTarget instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject mPlayer;
+
+}
