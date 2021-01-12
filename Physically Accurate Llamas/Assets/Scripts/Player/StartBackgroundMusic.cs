8c430e0b060ad2dc1b5359ba3ab9dc77eba0b80d
treecreeper123 <treecreeper123@gmail.com>
2021-01-12 12:59:56 +0000

    Background Music - Quick Fixes

---
+++ Physically Accurate Llamas/Assets/Scripts/Player/StartBackgroundMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBackgroundMusic : MonoBehaviour
+{
+    [SerializeField] private AudioClip mBackgroundMusic;
+
+    private void Start()
+    {
+        GameObject AudioObject = AudioSystem.Instance.GetAudioObject();
+        AudioSource ObjectAudioSource = AudioObject.GetComponent<AudioSource>();
+
+        AudioObject.SetActive(true);
+
+        ObjectAudioSource.loop = true;
+        ObjectAudioSource.clip = mBackgroundMusic;
+
+        ObjectAudioSource.volume = 0.1f;
+
+        ObjectAudioSource.Play();
+    }
+}
